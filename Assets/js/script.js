7c081b244605b5521620ecd89570d00f67255322
lilgionate <140750280+lilgionate@users.noreply.github.com>
2023-11-06 00:13:01 -0800

    pushing code to github

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,69 +1,95 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+var today = dayjs().format("dddd, MMMM DD YYYY ");
+var now = dayjs().format("H A");
 
-$(document).ready(function () {
+// current day
 
-  // Display current day
-    var currentDay = dayjs().format('MMMM DD, YYYY');
-    $("#currentDay").text(currentDay);
+$("#currentDay").text(today);
 
 
-  // Generate time blocks  
-    var timeBlocksDiv = $('#time-block');
-    for (var hour = 9; hour <= 17; hour++) {
-      var timeBlockDiv = $("<div>").addClass("timeblock");
-      var time = (hour % 12) + (hour >= 12 ? "pm" : "am");
-      timeBlockDiv.text(time);
+/* planWorkday entries for each hour of the workday */
 
-  // Add color coding based on time 
-      var currentHour = dayjs().hour();
-      if (hour < currentHour) {
-        timeBlockDiv.addClass("past");
-      } else if 
-        (hour === currentHour) {
-          timeBlockDiv.addClass("present");
-        } else { 
-          timeBlockDiv.addClass("future");
-        }
+var planWorkday = [
+  {time: "9 AM",
+     event: "" },
+  {time: "10 AM",
+     event: "" },
+  {time: "11 AM",
+     event: "" },
+  {time: "12 PM",
+     event: "" },
+  {time: "1 PM",
+     event: "" },
+  {time: "2 PM",
+     event: "" },
+  {time: "3 PM",
+     event: "" },
+  {time: "4 PM",
+     event: "" },
+  {time: "5 PM",
+     event: "" },
+];
 
-        timeBlocksDiv.append(timeBlockDiv);
-      }
+/* Local Storage check */
+var workEvents = JSON.parse(localStorage.getItem("workDay"));
+if (workEvents) {
+  planWorkday = workEvents;
+}
 
 
-      // Add event handling to save text to localStorage
+/* Create rows */
 
-      $(".timeblock").click(function() {
-        var eventText = prompt("Enter your event for this timeblock:");
-        localStorage.setItem("event" + $(this).index(), eventText);
-      });
+planWorkday.forEach(function(timeBlock, index) {
+  var timeLabel = timeBlock.time;
+  var blockColor = colorRow(timeLabel);
+  var row = 
+          '<div class="time-block" id="' +
+          index +
+          '"><div class="row no-gutters input-group"><div class="col-sm col-lg-1 input-group-prepend hour justify-content-sm-end pr-3 pt-3">' +
+          timeLabel + 
+          '</div><textarea class="form-control ' +
+          blockColor + 
+          '">' +
+          timeBlock.event +
+          '</textarea><div class"col-sm col-lg-1 input-group-append"><button class="saveBtn btn-block" type="submit"><i class="fas fa-save"></i></button></div></div></div>';
 
-
-      // Retrieve saved events from localStorage
-
-      $(".timeblock").each(function(index) {
-        var savedEvent = localStorage.getItem("event" + index);
-        if (savedEvent !== null) {
-          $(this).text($(this).text() + " - " + savedEvent);
-        }
-      });
-
+       /* Adding rows to container div */
+       
+       $(".container").append(row);
 });
-// TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+
+/* Color rows based on current time */
+
+function colorRow(time) {
+  var planNow = dayjs(now, "H A");
+  var planEntry = dayjs(time, "H A");
+  if (planNow.isBefore(planEntry) === true) {
+    return "future";
+  } else if (planNow.isAfter(planEntry) === true) {
+    return "past";
+  } else {
+    return "present";
+  }
+
+}
+
+
+/* Save events */
+
+$(".saveBtn").on("click", function(){
+  var blockID = parseInt(
+    $(this)
+    .closest(".time-block")
+    .attr("id")
+  );
+  var userEntry = $.trim(
+    $(this)
+    .parent()
+    .siblings("textarea")
+    .val()
+  );
+  planWorkday[blockID].event = userEntry;
+
+  /* Set local storage */
+  localStorage.setItem("workDay", JSON.stringify(planWorkday));
+});
